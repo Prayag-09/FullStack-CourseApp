625ae98dd3a05c156bfa6a8601cd8311b2badb35
Prayag-09 <91512981+Prayag-09@users.noreply.github.com>
2023-10-27 15:29:15 +0530

    mongoose schema & jwt

--- courseapp.js
+++ courseapp.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jwt');
+
 const app = express();
 app.use(express.json());
-const port = 3000;
+
+const secretCode = '';
+const user = mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const adminSchema = mongoose.Schema({
+    Admin: String,
+    Password: String 
+});
